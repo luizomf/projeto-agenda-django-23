b9a9ed118d510713e32739a348050f68fbafe5a8
Luiz Otávio <luizomf@gmail.com>
2023-04-29 20:03:27 -0300

    Injetando contatos no contexto de index.html

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from contact.models import Contact
+
 
 def index(request):
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
         'contact/index.html',
+        context
     )
