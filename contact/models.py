b4d23ec1cf896c2c52eef0af6d17b7252c3caeba
Luiz Otávio <luizomf@gmail.com>
2023-04-29 16:24:44 -0300

    Criando um ImageField e configurando MEDIA_URL e MEDIA_ROOT no Django

--- contact/models.py
+++ contact/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
 
 
 class Contact(models.Model):
@@ -17,6 +17,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
--- project/settings.py
+++ project/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
