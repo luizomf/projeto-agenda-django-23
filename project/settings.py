e7c99f860cb559459b398593ca02d4eb5280c2c6
Luiz Otávio <luizomf@gmail.com>
2023-04-29 19:05:19 -0300

    Usando local_settings.py para sobrescrever variáveis de settings.py

--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
