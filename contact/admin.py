7c43784f5a46ec92181a1cc2080bcc4422524a4c
Luiz Otávio <luizomf@gmail.com>
2023-04-29 16:52:04 -0300

    ForeignKey - criando o Model Category e fazendo a ligação com chave estrangeira

--- contact/admin.py
+++ contact/admin.py
@@ -13,3 +13,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -10,6 +10,13 @@ from django.utils import timezone
 # owner (foreign key)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -19,6 +26,11 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
