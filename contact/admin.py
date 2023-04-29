40f312f4107c6bf15c7ac3b6d776c9dd150cdec4
Luiz Otávio <luizomf@gmail.com>
2023-04-29 14:35:40 -0300

    Customizando as opções admin.ModelAdmin (Área administrativa do Django)

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
