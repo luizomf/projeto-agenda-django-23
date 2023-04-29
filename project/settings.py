09aa1170f5fa107f4e336d58ae0b05d6fe68dc1a
Luiz Otávio <luizomf@gmail.com>
2023-04-29 18:31:45 -0300

    Script Python que gera dados aleatórios no Django com o Faker

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+# type: ignore
+# flake8: noqa
 """
 Django settings for project project.
 
---
+++ utils/create_contacts.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+from random import choice
+
+import django
+from django.conf import settings
+
+DJANGO_BASE_DIR = Path(__file__).parent.parent
+NUMBER_OF_OBJECTS = 1000
+
+sys.path.append(str(DJANGO_BASE_DIR))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
+settings.USE_TZ = False
+
+django.setup()
+
+if __name__ == '__main__':
+    import faker
+
+    from contact.models import Category, Contact
+
+    Contact.objects.all().delete()
+    Category.objects.all().delete()
+
+    fake = faker.Faker('pt_BR')
+    categories = ['Amigos', 'Família', 'Conhecidos']
+
+    django_categories = [Category(name=name) for name in categories]
+
+    for category in django_categories:
+        category.save()
+
+    django_contacts = []
+
+    for _ in range(NUMBER_OF_OBJECTS):
+        profile = fake.profile()
+        email = profile['mail']
+        first_name, last_name = profile['name'].split(' ', 1)
+        phone = fake.phone_number()
+        created_date: datetime = fake.date_this_year()
+        description = fake.text(max_nb_chars=100)
+        category = choice(django_categories)
+
+        django_contacts.append(
+            Contact(
+                first_name=first_name,
+                last_name=last_name,
+                phone=phone,
+                email=email,
+                created_date=created_date,
+                description=description,
+                category=category,
+            )
+        )
+
+    if len(django_contacts) > 0:
+        Contact.objects.bulk_create(django_contacts)
