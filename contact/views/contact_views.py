4c25a2f26215cfe5d24896118edb892d183184f2
Luiz Otávio <luizomf@gmail.com>
2023-04-29 21:02:53 -0300

    Manipulando QuerySets com filter, order_by e fatiamento

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,7 +4,9 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
