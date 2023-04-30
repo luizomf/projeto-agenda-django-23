461936753a7490331d1393e85d5e0275a8a519a2
Luiz Otávio <luizomf@gmail.com>
2023-04-30 00:26:40 -0300

    Filtrando valores com Q e OR para o campo de pesquisa

--- contact/urls.py
+++ contact/urls.py
@@ -6,5 +6,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
+    path('search/', views.search, name='search'),  # type:ignore
     path('', views.index, name='index'),  # type:ignore
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
@@ -11,6 +12,34 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
     }
 
     return render(
