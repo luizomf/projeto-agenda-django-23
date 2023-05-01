366d3951de307b3eaa701ebb1cf32a5bedd82110
Luiz Otávio <luizomf@gmail.com>
2023-05-01 09:35:30 -0300

    Padrão em URLs de CRUD

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,4 @@
+# type:ignore
 from django.urls import path
 
 from contact import views
@@ -5,7 +6,9 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
