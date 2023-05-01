0487a6ff76c0bcaba301a5171c3fe014e6fa0ca4
Luiz Otávio <luizomf@gmail.com>
2023-05-01 11:17:25 -0300

    Usando request.method e request.POST para saber quando o formulário é postado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,11 +6,20 @@ from contact.models import Contact
 
 
 def create(request):
-    post = request.POST
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
 
     context = {
 
     }
+
+    print()
+    print(request.method)
+    print()
 
     return render(
         request,
