b3c0bec4101458535e3a97c89d8132c8d607155d
Luiz Otávio <luizomf@gmail.com>
2023-05-01 17:42:26 -0300

    Criando a view, url e template para "delete" (apagar contatos)

--- contact/urls.py
+++ contact/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -72,3 +72,23 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
+    )
