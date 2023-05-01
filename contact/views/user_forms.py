e4ab13d07ef155180a472233524a31eb6680bfaf
Luiz Otávio <luizomf@gmail.com>
2023-05-01 19:53:56 -0300

    Usando django.contrib.messages para enviar mensagens

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
 from contact.forms import RegisterForm
 
@@ -11,6 +12,8 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
 
     return render(
         request,
